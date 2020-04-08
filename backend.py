014addd258f7835de34c7a2acd58d0f2d05fd08d
Harshit <harshitkawatra08@gmail.com>
2020-04-08 16:04:18 +0530

    reorganized stuff

--- backend.py
+++ backend.py
@@ -1,5 +1,3 @@
-import tkinter as tk
-from tkinter import *
 MORSE_CODE_DICT = { 'A':'.-', 'B':'-...', 
                     'C':'-.-.', 'D':'-..', 'E':'.', 
                     'F':'..-.', 'G':'--.', 'H':'....', 
@@ -61,31 +59,3 @@ def decrypt(message): #morse to eng
 if __name__=='__main__':
     main()'''
  
-m=tk.Tk()
-m.title('MORSE CODE TRANSLATOR ')
-a=Label(m,text='MORSE CODE TRANSLATOR',font=("Times New Roman",20)).grid(row=0)
-#ENG TO MORSE
-b=Label(m,text='Enter sentance to be translated to morse').grid(row=2)
-e1=Entry(m)
-e1.grid(row=2, column=1)
-def clicked():
-    result=encrypt(e1.get())
-    output="The sentence in morse code is"+result
-    c=Label(m,text=output).grid(row=3,column=0)
-
-bt=Button(m,text="Convert",command=clicked)
-bt.grid(row=2,column=2)
-#MORSE TO ENG
-d=Label(m,text='Enter morse code to be translated to english').grid(row=5)
-e2=Entry(m)
-e2.grid(row=5, column=1)
-def clicked():
-    result=decrypt(e2.get())
-    output="The sentence in English is"+result
-    c=Label(m,text=output).grid(row=6,column=0)
-
-bt=Button(m,text="Convert",command=clicked)
-bt.grid(row=5,column=2)
-
-
-m.mainloop()
